40229d69492d3d8b1baf90ed63aee306063b6049
liu777702 <liu777702@gmail.com>
2017-03-06 11:22:25 +0800

    初始化專案

--- CS280-Homework-1.2/Pokemon.cs
+++ CS280-Homework-1.2/Pokemon.cs
@@ -8,8 +8,10 @@ namespace CS280_Homework_1._2
 {
     public class Pokemon
     {
+        public float Number;
         public float Height;
         public float Weight;
+        public string Name;
         public string Abilities;
         public string Category;
         public string Gender;
--- CS280-Homework-1.2/Program.cs
+++ CS280-Homework-1.2/Program.cs
@@ -11,13 +11,15 @@ namespace CS280_Homework_1._2
         private static void Main(string[] args)
         {
             Pokemon pokemon = new Pokemon();
+            pokemon.Number = 004;
             pokemon.Height = 2.0f;
             pokemon.Weight = 12.7f;
+            pokemon.Name = "Charmander";
             pokemon.Category = "Lizard";
             pokemon.Abilities = "Blaze";
             pokemon.Gender = "M";
-            Console.WriteLine("身高:{0}公尺, 體重{1}磅",
-                pokemon.Height, pokemon.Weight);
+            Console.WriteLine("編號:{0}號,名字:{1},身高:{2}公尺, 體重{3}磅",
+                pokemon.Number, pokemon.Name, pokemon.Height, pokemon.Weight);
         }
     }
 }
